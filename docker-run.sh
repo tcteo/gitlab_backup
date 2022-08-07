a1d71e50421840e3015ece66ca8356773774527b
TC Teo <tsechin@tsechin.com>
2022-08-07 16:47:17 +1000

    shell script for testing

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,7 @@
 docker run -it --rm \
+-e GITLAB_USER="tcteo" \
 -e GITLAB_TOKEN="$(cat access_token.txt)" \
-gitlab_backup:latest
+-v $(pwd)/tmp:/data \
+gitlab_backup:latest /data/sync
 
 #--mount type=bind,source=$(pwd)/access_token.txt,target=/access_token.txt gitlab_backup:latest \
