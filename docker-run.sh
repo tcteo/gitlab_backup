210ef737f8f43a5098ecdfede63a2c7da7ec761b
TC Teo <tsechin@tsechin.com>
2022-08-07 21:09:28 +1000

    delete commented line

--- docker-run.sh
+++ docker-run.sh
@@ -3,5 +3,3 @@ docker run -it --rm \
 -e GITLAB_TOKEN="$(cat access_token.txt)" \
 -v $(pwd)/tmp:/data \
 gitlab_backup:latest /data/sync
-
-#--mount type=bind,source=$(pwd)/access_token.txt,target=/access_token.txt gitlab_backup:latest \
