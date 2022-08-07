8a98788971f30ad271e72ec76e486285a5cd6ba4
TC Teo <tsechin@tsechin.com>
2022-08-07 16:46:43 +1000

    Implement backup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:3.16
 RUN apk add --no-cache git glab curl jq bash
 COPY main.sh /main.sh
+RUN mkdir /data
+VOLUME ["/data"]
 
 ENTRYPOINT ["/main.sh"]
--- main.sh
+++ main.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
+set -e
 
-# - List projects I'm a member of
+# - List projects the token owner is a member of
 # - Use jq to convert it to a tab separated format
-# - Call a function for each repo
+# - Back up each repo to dst_base specified in $1
+
+if [ -z "${1}" ]; then
+  echo 'no dst dir'
+  exit 1
+fi
+dst_base="${1}"
+
+if [ -z "${GITLAB_USER}" ]; then
+  echo 'no gitlab username'
+  exit 1
+fi
+if [ -z "${GITLAB_TOKEN}" ]; then
+  echo 'no gitlab token'
+  exit 1
+fi
+
+do_backup () {
+  namespace="${1}"
+  repo="${2}"
+  sshurl="${3}"
+  httpurl="${4}"
+
+  echo "do_backup: namespace=${namespace} repo=${repo} sshurl=${sshurl} httpurl=${httpurl}"
+  dst="${dst_base}/${namespace}/${repo}"
+  echo "dst: ${dst}"
+
+  httpurl_auth="$( echo "${httpurl}" | sed -e "s/https:\/\//https:\/\/${GITLAB_USER}:${GITLAB_TOKEN}@/g" )"
+
+  wd="$(pwd)"
+  if [ ! -d "${dst}" ]; then
+    mkdir -p "${dst}"
+    echo "cloning..."
+    git clone --mirror "${httpurl_auth}" "${dst}"
+  else
+    cd "${dst}"
+    echo "fetching..."
+    git fetch "${httpurl_auth}" || \
+    (echo "fetch failed, cloning instead..."; git clone --mirror "${httpurl_auth}" "${dst}")
+    cd "${wd}"
+  fi
+  
+  echo '----------------------------'
+}
 
 glab api graphql -f query='
 query {
@@ -37,6 +81,6 @@ query {
 ' \
 | (
   while read -r namespace repo sshurl httpurl; do
-    echo "namespace=${namespace} repo=${repo} sshurl=${sshurl} httpurl=${httpurl}"
+    do_backup "${namespace}" "${repo}" "${sshurl}" "${httpurl}"
   done
 )
